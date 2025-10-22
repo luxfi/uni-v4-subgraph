834ede6f003591b7484d0408e8c3f6fd6d442d8a
matteen <matteen.mobasheran@uniswap.org>
2025-10-22 11:44:39 -0400

    v4 celo subgraph (#59)

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -24,6 +24,8 @@ const MAINNET_NETWORK_NAME = 'mainnet'
 const BLAST_MAINNET_NETWORK_NAME = 'blast-mainnet'
 const UNICHAIN_MAINNET_NETWORK_NAME = 'unichain-mainnet'
 const SONEIUM_MAINNET_NETWORK_NAME = 'soneium-mainnet'
+const CELO_NETWORK_NAME = 'celo'
+
 // Note: All token and pool addresses should be lowercased!
 export class SubgraphConfig {
   // deployment address
@@ -579,6 +581,42 @@ export function getSubgraphConfig(): SubgraphConfig {
       nativeTokenDetails: {
         symbol: 'ETH',
         name: 'Ethereum',
+        decimals: BigInt.fromI32(18),
+      },
+    }
+  } else if (selectedNetwork == CELO_NETWORK_NAME) {
+    return {
+      poolManagerAddress: '0x288dc841a52fca2707c6947b3a777c5e56cd87bc',
+      stablecoinWrappedNativePoolId: '0x29aa9a73eedb0324148d5e43c5ebf2d479fbf04abea11e0d5afa7143387e30c6', // CELO-USDT
+      stablecoinIsToken0: false,
+      wrappedNativeAddress: '0x471ece3750da237f93b8e339c536989b8978a438', // CELO
+      minimumNativeLocked: BigDecimal.fromString('3600'),
+      stablecoinAddresses: [
+        '0x765de816845861e75a25fca122bb6898b8b1282a', // CUSD
+        '0xef4229c8c3250c675f21bcefa42f58efbff6002a', // Bridged USDC
+        '0xceba9300f2b948710d2653dd7b07f33a8b32118c', // Native USDC,
+        '0x48065fbbe25f71c9282ddf5e1cd6d6a887483d5e', // USDT
+      ],
+      whitelistTokens: [
+        '0x471ece3750da237f93b8e339c536989b8978a438', // CELO
+        '0x765de816845861e75a25fca122bb6898b8b1282a', // CUSD
+        '0xef4229c8c3250c675f21bcefa42f58efbff6002a', // Bridged USDC
+        '0xceba9300f2b948710d2653dd7b07f33a8b32118c', // Native USDC
+        '0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73', // CEUR
+        '0xe8537a3d056da446677b9e9d6c5db704eaab4787', // CREAL
+        '0x46c9757c5497c5b1f2eb73ae79b6b67d119b0b58', // PACT
+        '0x17700282592d6917f6a73d0bf8accf4d578c131e', // MOO
+        '0x66803fb87abd4aac3cbb3fad7c3aa01f6f3fb207', // Portal Eth
+        '0xbaab46e28388d2779e6e31fd00cf0e5ad95e327b', // WBTC,
+        '0xd221812de1bd094f35587ee8e174b07b6167d9af', // WETH
+        '0x48065fbbe25f71c9282ddf5e1cd6d6a887483d5e', // USDT
+      ],
+      tokenOverrides: [],
+      poolsToSkip: [],
+      poolMappings: [],
+      nativeTokenDetails: {
+        symbol: 'CELO',
+        name: 'Celo',
         decimals: BigInt.fromI32(18),
       },
     }
